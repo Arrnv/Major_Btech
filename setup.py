e248d883870c701944237c661bcbdc49c12075f8
Arnav Lahane <119531646+Arrnv@users.noreply.github.com>
2024-12-14 20:02:11 +0530

    dvc

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ def get_requirements(filepath:str)->List[str]:
 setup(
     name="Major_project",
     version="0.0.1",
-    author="Arnav, Pratham",
-    author_email="lahanearnav9@gmail.com",
+    author="Arnav, Pratham,Maitreyee, Prachi ",
+    author_email="lahanearnav9@gmail.com,prathampatharkar09@gmail.com,maitreyee08deshmukh@gmail.com, prachisatpute25@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )

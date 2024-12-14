b3a5e74825dd3bb23ee7a535cd1fde75ddcd5985
Arnav Lahane <119531646+Arrnv@users.noreply.github.com>
2024-12-14 20:58:58 +0530

    Data transofrmation

--- src/MajorBtech/components/dataInject.py
+++ src/MajorBtech/components/dataInject.py
@@ -3,15 +3,14 @@ import sys
 from src.MajorBtech.exception import CustomException
 from src.MajorBtech.logger import logging
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.MajorBtech.utils import read_sql_data
 
 @dataclass
 class DataInjectionConfig:
     raw_data_path:str=os.path.join('artifact','raw.csv')
-    train_data_path:str=os.path.join('artifact','train.csv')
-    test_data_path:str=os.path.join('artifact','test.csv')
+    # train_data_path:str=os.path.join('artifact','train.csv')
+    # test_data_path:str=os.path.join('artifact','test.csv')
     
 
 class DataInjection:
@@ -23,13 +22,12 @@ class DataInjection:
             logging.info("reading from mysql database")
             os.makedirs(os.path.dirname(self.injection_config.raw_data_path),exist_ok=True)
             df.to_csv(self.injection_config.raw_data_path,index = False, header=True)
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=11)
-            train_set.to_csv(self.injection_config.train_data_path,index = False, header=True)
-            test_set.to_csv(self.injection_config.test_data_path,index = False, header=True)
+            # train_set, test_set = train_test_split(df, test_size=0.2, random_state=11)
+            # train_set.to_csv(self.injection_config.train_data_path,index = False, header=True)
+            # test_set.to_csv(self.injection_config.test_data_path,index = False, header=True)
             logging.info("Data Injection Complete")
             return (
-                self.injection_config.train_data_path,
-                self.injection_config.test_data_path
+                self.injection_config.raw_data_path
             )
         except Exception as e:
             raise CustomException(e,sys)
